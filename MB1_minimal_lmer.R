18d534db9e05d721ba314f98e6b1f1a4312f0708
Christina Bergmann <chbergma@uos.de>
2020-02-10 11:28:42 +0100

    Add minimal MB1 analysis

---
+++ MB1_minimal_lmer.R
@@ -0,0 +1,37 @@
+library(lme4)
+library(tidyverse)
+library(here)
+library(lmerTest)
+
+
+d <- read_csv("https://raw.githubusercontent.com/manybabies/mb1-analysis-public/master/processed_data/03_data_trial_main.csv", 
+              na = c("NA", "N/A")) %>%
+  mutate(method = case_when(
+    method == "singlescreen" ~ "Central fixation",
+    method == "eyetracking" ~ "Eye tracking",
+    method == "hpp" ~ "HPP",
+    TRUE ~ method)) 
+
+
+ordered_ages <- c("3-6 mo", "6-9 mo", "9-12 mo", "12-15 mo")
+d$age_group <- fct_relevel(d$age_group, ordered_ages)
+
+
+d_lmer <- d %>%
+  filter(trial_type != "train") %>%
+  mutate(log_lt = log(looking_time),
+         age_mo = scale(age_mo, scale = FALSE),
+         trial_num = trial_num, 
+         item = paste0(stimulus_num, trial_type)) %>%
+  filter(!is.na(log_lt), !is.infinite(log_lt))
+
+mod_lmer <- lmer(log_lt ~ trial_type * method +
+                   trial_type * trial_num +
+                   age_mo * trial_num +
+                   trial_type * age_mo * nae +
+                   (1 | subid_unique) +
+                   (1 | item) + 
+                   (1 | lab), 
+                 data = d_lmer)
+
+summary(mod_lmer)
