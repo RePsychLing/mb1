af54073d56e3cc31323a0c24bb0d236dea78ddf6
Christina Bergmann <chbergma@uos.de>
2020-02-11 16:30:26 +0100

    Add dataset, comment code

--- MB1_minimal_lmer.R
+++ MB1_minimal_lmer.R
@@ -1,8 +1,14 @@
 library(lme4)
 library(tidyverse)
 
+# This is a minimal script that contains all relevant code to replicate the main LMM of 
+# The ManyBabies Consortium. (2019). "Quantifying sources of variability in infancy research using the infant-directed speech preference." In press at Advances in Methods and Practices in Psychological Science (AMPPS).
 
-d <- read_csv("https://raw.githubusercontent.com/manybabies/mb1-analysis-public/master/processed_data/03_data_trial_main.csv", 
+# For more information, see https://github.com/manybabies/mb1-analysis-public
+
+
+# Read in data and recode method
+d <- read_csv("03_data_trial_main.csv", 
               na = c("NA", "N/A")) %>%
   mutate(method = case_when(
     method == "singlescreen" ~ "Central fixation",
@@ -10,10 +16,15 @@ d <- read_csv("https://raw.githubusercontent.com/manybabies/mb1-analysis-public/
     method == "hpp" ~ "HPP",
     TRUE ~ method)) 
 
-
+# Order age to ensure that they appear chronologically, this is not used in the model but kept for completeness
 ordered_ages <- c("3-6 mo", "6-9 mo", "9-12 mo", "12-15 mo")
 d$age_group <- fct_relevel(d$age_group, ordered_ages)
 
+# Construct a dataset for fitting the model: 
+#   exclude training trials (redundant, not in the dataset)
+#   subtract mean age from participants' age in months 
+#   construct an item variable 
+#   and remove missing data
 
 d_lmer <- d %>%
   filter(trial_type != "train") %>%
@@ -22,6 +33,9 @@ d_lmer <- d %>%
          trial_num = trial_num, 
          item = paste0(stimulus_num, trial_type)) %>%
   filter(!is.na(log_lt), !is.infinite(log_lt))
+
+
+# Fit the pruned model and summarize
 
 mod_lmer <- lmer(log_lt ~ trial_type * method +
                    trial_type * trial_num +
@@ -34,7 +48,7 @@ mod_lmer <- lmer(log_lt ~ trial_type * method +
 
 summary(mod_lmer)
 
-
+### This is the preregistered model before pruning, which throws singularity warnings
 #original_mod_lmer <- lmer(log_lt ~ trial_type * method +
 #                   trial_type * trial_num +
 #                   age_mo * trial_num +
